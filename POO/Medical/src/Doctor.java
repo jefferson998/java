0b363ff58310212ebf3a12cd7d8695a7113b6d00
jefferson998 <pazjefferson11216@gmail.com>
2021-06-04 22:07:39 -0500

    cambios antes de modulalizar

--- POO/Medical/src/Doctor.java
+++ POO/Medical/src/Doctor.java
@@ -1,7 +1,20 @@
 public class Doctor {
-    int id;
+    static int id=0;
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+
+    Doctor (String name){
+        System.out.println("El nobre del doctor asiganado es: "+name);
+
+    }
+    public void showId(){
+        System.out.println("ID Dorctor: "+id);
+    }
 
 
     //comportamientos
@@ -9,4 +22,7 @@ public class Doctor {
     public void showName(){
         System.out.println(name);
     }
+
+
+
 }
--- POO/Medical/src/Main.java
+++ POO/Medical/src/Main.java
@@ -1,10 +1,21 @@
+import static ui.UImenu.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Jefferson Paz");
         myDoctor.name = "Jefferson";
         myDoctor.showName();
+        myDoctor.showId();
+        //System.out.println(Doctor.id);
+
+//
+        Doctor MyDoctorJeff = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
 
     }
 

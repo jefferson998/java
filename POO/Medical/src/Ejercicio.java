445ee2bbe0537f602448d1a13ad21a0912ee7119
jefferson998 <pazjefferson11216@gmail.com>
2021-06-05 11:30:37 -0500

    cambios

---
+++ POO/Medical/src/Ejercicio.java
@@ -0,0 +1,10 @@
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        byte i =1;
+        byte j =1;
+        byte k= (byte) (i+j);
+        System.out.println(k);
+
+    }
+}
